ee52fcfdc62fe1eead4be80c2cc328881eeff48f
Ghislain Lévêque <ghislain.leveque@gmail.com>
2022-07-06 19:19:26 +0200

    Improve sequencer

--- sequencer.js
+++ sequencer.js
@@ -6,12 +6,31 @@ class Sequencer {
   constructor() {
     this.position = 0;
     this.progression = [1, 1, 4, 6, 3, 5];
+    this.inputs = {
+      progression: sequencerInput
+    };
+    this.setupGui();
+    this.refreshDisplay();
+  }
+
+  setupGui() {
+    if (this.inputs.progression) {
+      this.inputs.progression.onchange = () => {
+        this.progression = this.inputs.progression.value.split(" ").map((val) => parseInt(val, 10));
+      };
+    }
+  }
+
+  refreshDisplay() {
+    if (this.inputs.progression) this.inputs.progression.value = this.progression.join(" ");
   }
 
   tick() {
-    console.log("sequencer tick");
-    if (window.masterClock % 48 === 0) {
-      setChord(this.progression[this.position % this.progression.length]);
+    if (window.masterClock % 96 === 0 && this.progression.length > 0) {
+      const newChord = this.progression[this.position % this.progression.length]
+      if (newChord) {
+        setChord(newChord);
+      }
       this.position += 1;
     }
   }
