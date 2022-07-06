9f69e2ceb39e88d950a774dfae6446abefd5002c
Ghislain Lévêque <ghislain.leveque@gmail.com>
2022-07-06 23:21:20 +0200

    Drone preset

--- song1.js
+++ song1.js
@@ -6,29 +6,30 @@ const strum = PresetTrack(0, 55, 100, "pad", "widechord");
 strum.strumDelay = 50;
 
 export const tracks = [
+  // Drone
+  PresetTrack(3, 100, 100, "pad", "drone"),
   // Chords ("pad")
   strum,
   // Bassline
-  /*
-  PresetTrack(0, 55, 65, "bass", "widechord"),
+  PresetTrack(2, 55, 65, "bass", "widechord"),
   // Lead
-  PresetTrack(1, 60, "lead", "varponderatedscale"),
+  PresetTrack(1, 60, 45, "lead", "varponderatedscale"),
   // drums
   // 36 = kick
-  PresetTrack(9, 50, "drums", "kick13"),
+  PresetTrack(10, 50, 30, "drums", "kick13"),
   // 37 = rim
   // 39 = clap
   // 41 = low snare or kick
   // 38 40 43 45 = snare
-  PresetTrack(9, 50, "drums", "snare24"),
+  PresetTrack(9, 50, 30, "drums", "snare24"),
   // 47 48 50 = tom
   // 46 51 53 = cymbal
   // 54 = tambourin
   // 42 = CH
-  PresetTrack(9, 100, "drums", "chhn"),
+  PresetTrack(9, 100, 30, "drums", "chhn"),
   // 44 55 = OH
   // 49 57 = crash
-  new Track(9, 0, 60, 8, [100, 30, 50, 20], [46]),
+  new Track(9, 0, 30, 60, 8, [100, 30, 50, 20], [46]),
   // 59 = ride
-  */
+  /* */
 ];
