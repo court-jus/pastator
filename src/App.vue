f7175cd68b2a50daac5629c55123989a78557f59
Ghislain Leveque <ghislain.leveque@cenareo.com>
2022-08-05 13:47:11 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ interface AppData {
   midiSystem?: MIDIAccess
   clock: number
   tour: Tour
+  showMidiDevices: boolean
 }
 
 export default {
@@ -25,6 +26,7 @@ export default {
       midiOutputDevice: undefined,
       midiCCDevice: undefined,
       midiNotesDevice: undefined,
+      showMidiDevices: true,
       midiSystem: undefined,
       clock: 0,
       tour: {
@@ -107,10 +109,10 @@ export default {
 </script>
 
 <template>
-  <div class="row">
-    <div class="col">
+  <nav class="navbar sticky-top navbar-light bg-light justify-content-between px-3 py-0">
+    <a class="navbar-brand pt-0 pb-0" href="#">
       <div class="logo-container clickable" @click="onLogoClicked" title="Click to launch the welcome tour.">
-        <svg class="logo-led" width="125" height="125" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+        <svg class="logo-led" width="36" height="36" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
           <circle cx="16" cy="80" r="10" :fill="clock ? (clock % 96 < 48 ? 'green' : 'red') : 'gray'" opacity="0.7" />
           <circle cx="239" cy="84" r="10" :fill="clock ? (clock % 96 >= 48 ? 'green' : 'red') : 'gray'" opacity="0.7" />
           <path :fill="(clock && clock % 96 < 24) ? 'green' : 'transparent'"
@@ -121,34 +123,37 @@ export default {
             d="M 169.32031 115.19727 C 156.93999 115.17207 143.45164 115.37826 129.79883 116.03516 L 129.79883 185.61523 C 132.30134 185.70344 134.73892 185.64346 137.06836 185.38672 C 148.02393 184.17922 159.63117 179.14963 169.97656 171.9082 L 169.97656 115.20312 C 169.74704 115.2025 169.55061 115.19773 169.32031 115.19727 z " />
           <path :fill="(clock && 72 <= clock % 96) ? 'green' : 'transparent'"
             d="M 169.12305 115.19727 L 169.12305 172.49023 C 189.84401 158.32414 205.76621 135.21221 201.72852 115.69922 C 192.54378 115.46249 181.37048 115.21986 169.12305 115.19727 z " />
-
         </svg>
-        <img alt="Vue logo" class="logo" src="/logo256.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="/logo256.png" width="36" height="36" />
+      </div>
+      <div class="d-inline-block mt-1 align-top">Pastator!</div>
+    </a>
+    <ul class="nav nav-pills">
+      <button class="btn btn-secondary" @click="showMidiDevices = !showMidiDevices">MIDI Devices</button>
+    </ul>
+  </nav>
+  <div class="row">
+    <div class="col-6 offset-6" v-if="midiSystem && showMidiDevices">
+      <div>
+        <div class="row" id="midi-clock-selection">
+          <SelectMidiInput :label="'Clock'" :modelValue="midiClockDevice" @update:modelValue="newValue => midiClockDevice = newValue"
+            :midi="midiSystem" />
+        </div>
+        <div class="row" id="midi-out-selection">
+          <SelectMidiOutput :label="'Output'" :modelValue="midiOutputDevice" @update:modelValue="newValue => midiOutputDevice = newValue"
+            :midi="midiSystem" />
+        </div>
+        <div class="row" id="midi-cc-in" v-if="midiOutputDevice && midiClockDevice">
+          <SelectMidiInput :label="'Midi CC'" :modelValue="midiCCDevice" @update:modelValue="newValue => midiCCDevice = newValue"
+            :midi="midiSystem" />
+        </div>
+        <div class="row" id="midi-notes-in" v-if="midiOutputDevice && midiClockDevice">
+          <SelectMidiInput :label="'Midi Notes'" :modelValue="midiNotesDevice" @update:modelValue="newValue => midiNotesDevice = newValue"
+            :midi="midiSystem" />
+        </div>
       </div>
     </div>
-    <div class="col">
-      <h1>Pastator!</h1>
-    </div>
-    <div class="col" v-if="midiSystem">
-      <h1>Midi devices</h1>
-      <div class="row" id="midi-clock-selection">
-        <SelectMidiInput :label="'Clock'" :modelValue="midiClockDevice" @update:modelValue="newValue => midiClockDevice = newValue"
-          :midi="midiSystem" />
-      </div>
-      <div class="row" id="midi-out-selection">
-        <SelectMidiOutput :label="'Output'" :modelValue="midiOutputDevice" @update:modelValue="newValue => midiOutputDevice = newValue"
-          :midi="midiSystem" />
-      </div>
-      <div class="row" id="midi-cc-in" v-if="midiOutputDevice && midiClockDevice">
-        <SelectMidiInput :label="'Midi CC'" :modelValue="midiCCDevice" @update:modelValue="newValue => midiCCDevice = newValue"
-          :midi="midiSystem" />
-      </div>
-      <div class="row" id="midi-notes-in" v-if="midiOutputDevice && midiClockDevice">
-        <SelectMidiInput :label="'Midi Notes'" :modelValue="midiNotesDevice" @update:modelValue="newValue => midiNotesDevice = newValue"
-          :midi="midiSystem" />
-      </div>
-    </div>
-    <div class="col" v-else>
+    <div class="col" v-if="!midiSystem">
       Trying to get access to MIDI System...
     </div>
     <div class="col-12" v-if="midiOutputDevice && midiClockDevice">
@@ -166,6 +171,9 @@ export default {
 
 .logo-container {
   position: relative;
+  display: inline-block;
+  width: 36px;
+  height: 36px;
 }
 
 .logo-led {
