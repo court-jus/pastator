316f1bc7904fd6413ba74bbd4fdb03f8e06a0244
Ghislain Lévêque <ghislain.leveque@gmail.com>
2022-08-06 11:47:41 +0200

    Cleanup legacy folder

--- legacy/utils.js
+++
@@ -1,9 +0,0 @@
-export const download = (filename, data) => {
-    var element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8, " + encodeURIComponent(data));
-    element.setAttribute("download", filename);
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-  };
-  
